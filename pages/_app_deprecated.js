e28385f1f86e4634f349dfa15746ca3f8f19d73f
IzyAnalisAi <muhamadivan969@gmail.com>
2026-08-06 07:57:28 +0700

    Delete pages/_app_deprecated.js

--- pages/_app_deprecated.js
+++
@@ -1,4 +0,0 @@
-// This file is deprecated and kept as a backup. It should not be imported by the app.
-// If you want to restore the previous JS-based _app, copy contents from here to pages/_app.js.
-
-// Deprecated file - intentionally blank to avoid conflicts.
